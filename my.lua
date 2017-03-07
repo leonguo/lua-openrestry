a8b9ee2017821386b2ddac4c0341ae6577799706
wings <hotman8168@gmail.com>
2017-03-07 20:39:11 +0800

    lua

--- my.lua
+++ my.lua
@@ -1,9 +1,7 @@
 --
--- Created by IntelliJ IDEA.
--- User: Administrator
+-- User: wings
 -- Date: 2017/3/6
 -- Time: 17:57
--- To change this template use File | Settings | File Templates.
 --
 local foo = {}
 
